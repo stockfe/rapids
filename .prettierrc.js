2d499047f18ee30556a2ddb89bd5aaeb439ad6b8
kevinnwang <kevinnwang@tencent.com>
2018-08-24 00:29:33 +0800

    init

---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+// .prettierrc.js
+module.exports = {
+  // Specify the line length that the printer will wrap on.
+  // For readability we recommend against using more than 80 characters
+  "printWidth": 80,
+  // Specify the number of spaces per indentation-level.
+  "tabWidth": 2,
+  // Indent lines with tabs instead of spaces
+  "useTabs": false,
+  // Use single quotes instead of double quotes.
+  "singleQuote": true,
+  // Print semicolons at the ends of statements.
+  "semi": true,
+  // Print trailing commas wherever possible when multi-line. (A single-line array, for example, never gets trailing commas.)
+  "trailingComma": "es5",
+  // Print spaces between brackets in object literals.
+  "bracketSpacing": true,
+  // Include parentheses around a sole arrow function parameter.
+  "arrowParens": 'avoid',
+};
---
+++ README.md
@@ -0,0 +1,33 @@
+Rapids
+=======
+the mock service you wanted
+
+HIGHLIGHTS
+--------------
+
+### effect
+
+### time travel
+
+### workflow integration
+
+### api management
+
+QUICK STARTS
+-------------------
+
+
+CONCEPTS
+------------
+
+### C-S DESIGN
+
+### snapshot
+
+### actions
+
+### mutations
+
+CONTRIBUTIONS
+---------------
+donate me with a cup of coffe
